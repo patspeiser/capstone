a01b7f7d0dd848151d4b3bc6218b571d3ec8eb24
hekod777 <lifetimeinc@gmail.com>
2016-11-20 16:12:33 -0500

    added two search button to channel list
    search by tags, search by category. both uses URL so that the user can
    actually bookmark the search result.

--- browser/js/channels/channels.directive.js
+++ browser/js/channels/channels.directive.js
@@ -31,6 +31,20 @@ app.directive('channelDetail', function () {
 });
 
 
+
+       // $scope.joinRoom = function(roomname) { //join a room button
+       //      $scope.disableJoinRoom = true; // disables join room button after already joined
+       //      connection.sdpConstraints.mandatory = { //purely khan video stuff, NOT REUSABLE
+       //          OfferToReceiveAudio: true,
+       //          OfferToReceiveVideo: true
+       //      };
+       //      $rootScope.watching = true; //tells our app the user is watching something, REUSABLE
+       //      $rootScope.unwatching = roomname; //remembers the name the user is watching, so that we can reduce the view count when the user simply leaves the page, REUSABLE
+       //      BroadCastFactory.increaseView(roomname); //increase the view count of the channel by 1, REUSABLE
+       //      connection.join(roomname); //purely khan video stuff to join a room, NOT REUSABLE
+       //  };
+
+
 // var connection = new RTCMultiConnection(); //khan stuff to create a connection object, NOT REUSABLE
 
 //         // by default, socket.io server is assumed to be deployed on your own URL
