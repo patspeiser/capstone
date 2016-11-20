73891932d65c7c4cdeb4ced168b454fb674f5efa
tomtrasmontero <tomtrasmontero@gmail.com>
2016-11-20 17:10:54 -0500

    clean up broadcast, need to change into directive

--- browser/js/common/directives/live-broadcast/live-broadcast.state.js
+++ browser/js/common/directives/live-broadcast/live-broadcast.state.js
@@ -3,6 +3,9 @@ app.config(function ($stateProvider) {
     $stateProvider.state('broadcastLive', {
         url: '/broadcastLive',
         controller: 'BroadcastLiveCtrl',
-        templateUrl: 'js/common/directives/live-broadcast/live-broadcast.html'
+        templateUrl: 'js/common/directives/live-broadcast/live-broadcast.html',
+        resolve : {
+        	broadcastStatus: function(){ return true}
+        }
     });
 });
