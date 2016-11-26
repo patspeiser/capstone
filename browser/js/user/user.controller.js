875bf210522060898973d3b5b6087479bd1bf357
pspeiser <pat@patspeiser.com>
2016-11-26 16:33:52 -0500

    linked to profile page

---
+++ browser/js/user/user.controller.js
@@ -0,0 +1,16 @@
+app.config(function ($stateProvider) {
+
+    $stateProvider.state('user', {
+        url: '/user',
+        templateUrl: 'js/user/user.html',
+        controller: 'UserCtrl'
+    });
+
+});
+
+app.controller('UserCtrl', function ($scope, AuthService, $state) {
+	AuthService.getLoggedInUser()
+		.then(function(user){
+			$scope.user = user; 
+		});
+});
